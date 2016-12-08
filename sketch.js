d172a04b8c1f28b567f0175d7d430e50e9fadb6a
natashabadger <natasha.badger@gmail.com>
2016-12-08 14:05:22 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup(){
+  cnv = createCanvas(window.innerWidth, window.innerHeight);
+    cnv.parent('wrapperDiv');
+    cnv.position(20, 20);
+    background(65, 135, 163);
+}
+
+function draw(){
+  fill(200);
+ellipse(100, 2, 122);
+}
